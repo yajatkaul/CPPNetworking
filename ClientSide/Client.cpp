9a211c8e7864add2bc01a78c596928fc8be4c455
YajatKaul <68262974+yajatkaul@users.noreply.github.com>
2024-07-23 17:10:57 +0530

    first commit

---
+++ ClientSide/Client.cpp
@@ -0,0 +1,64 @@
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	SOCKET clientSocket;
+	int port = 55555;
+	WSADATA wsaData;
+	int wsaerr;
+	WORD wVersionRequested = MAKEWORD(2, 2);
+	wsaerr = WSAStartup(wVersionRequested, &wsaData);
+	if (wsaerr != 0) {
+		cout << "The Winsock dll not found!" << endl;
+		return 0;
+	}
+	else {
+		cout << "The Winsock dll found!" << endl;
+		cout << "The status: " << wsaData.szSystemStatus << endl;
+	}
+
+	clientSocket = INVALID_SOCKET;
+	clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (clientSocket == INVALID_SOCKET) {
+		cout << "Error at socket(): " << WSAGetLastError() << endl;
+		WSACleanup();
+		return 0;
+	}
+	else {
+		cout << "socket() is ok!" << endl;
+	}
+
+	sockaddr_in clientService;
+	clientService.sin_family = AF_INET;
+	InetPtonA(AF_INET, "127.0.0.1", &clientService.sin_addr.s_addr);
+	clientService.sin_port = htons(port);
+	if (connect(clientSocket, (SOCKADDR*)&clientService, sizeof(clientService)) == SOCKET_ERROR) {
+		cout << "Client: connect() - Failed to connect." << endl;
+		WSACleanup();
+		return 0;
+	}
+	else {
+		cout << "Client: connect() is ok." << endl;
+		cout << "Client: Can start sending and receiving data..." << endl;
+	}
+
+	char buffer[200];
+	cout << "Please enter a message: ";
+	cin.getline(buffer, 200);
+	int byteCount = send(clientSocket, buffer, 200, 0);
+	if (byteCount > 0) {
+		cout << "Message: " << buffer << endl;
+	}
+	else {
+		WSACleanup();
+	}
+
+
+	system("pause");
+	WSACleanup();
+	return 0;
+
+}
---
+++ ServerSide/Server.cpp
@@ -0,0 +1,70 @@
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	SOCKET serverSocket, acceptSocket;
+	int port = 55555;
+	WSADATA wsaData;
+	int wsaerr;
+	WORD wVersionRequired = MAKEWORD(2, 2);
+	wsaerr = WSAStartup(wVersionRequired, &wsaData);
+	if (wsaerr != 0) {
+		cout << "The Winsock dll not found" << endl;
+		return 0;
+	}
+	else {
+		cout << "The Winssock dll found!" << endl;
+		cout << "The status: " << wsaData.szSystemStatus << endl;
+	}
+
+	serverSocket = INVALID_SOCKET;
+	serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (serverSocket == INVALID_SOCKET) {
+		cout << "Error at socket(): " << WSAGetLastError() << endl;
+		WSACleanup();
+		return 0;
+	}
+	else {
+		cout << "socket() is ok!" << endl;
+	}
+
+	sockaddr_in service;
+	service.sin_family = AF_INET;
+	InetPtonA(AF_INET, "127.0.0.1", &service.sin_addr.s_addr);
+	service.sin_port = htons(port);
+	if (bind(serverSocket, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR) {
+		cout << "bind() failed: " << WSAGetLastError() << endl;
+		closesocket(serverSocket);
+		WSACleanup();
+		return 0;
+	}
+	else {
+		cout << "bind() is ok!" << endl;
+	}
+
+	if (listen(serverSocket, 1) == SOCKET_ERROR) {
+		cout << "listen(): Error listening on socket" << WSAGetLastError() << endl;
+	}
+	else {
+		cout << "listen() is ok, waiting for connections.." << endl;
+	}
+
+	acceptSocket = accept(serverSocket, NULL, NULL);
+	if (acceptSocket == INVALID_SOCKET) {
+		cout << "accept() failed: " << WSAGetLastError() << endl;
+		WSACleanup();
+		return -1;
+	}
+
+	cout << "Accepted connection" << endl;
+
+	char buffer[200];
+	int byteCount = send(acceptSocket, buffer, 200, 0);
+
+
+	system("pause");
+	WSACleanup();
+}
